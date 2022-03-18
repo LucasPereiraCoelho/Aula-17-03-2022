7a413a29f316591a8b54dd963e97620966b07e90
Lucas Pereira Coelho <101848408+LucasPereiraCoelho@users.noreply.github.com>
2022-03-17 22:55:57 -0300

    Add files via upload
    Exercícios feitos em aula.

---
+++ aula 17.03.py
@@ -0,0 +1,48 @@
+#Exemplos
+
+'''
+divisaoExata = 7 // 2
+print (divisaoExata)
+'''
+
+'''
+print("B" == "b")
+print(1 != 2)
+print(1 > 2)
+print(1 > 2)
+print(1 >= 1 <= 2)
+'''
+
+'''
+print("a" == "a" and "b" == "a")
+print("a" == "a" or "b" == "a")
+'''
+
+
+#Exercício 1 
+
+'''
+
+totalconta = float(input("Valor total da conta: "))
+
+partecarlos = totalconta // 3
+parteandre = totalconta // 3
+partefelipe = (totalconta - partecarlos) - parteandre
+
+print(partecarlos)
+print(parteandre)
+print(partefelipe)
+
+'''
+
+#Exercício 2
+
+'''
+
+valor1 = int(input("informe um valor: "))
+
+valor2 = int(input("informe outro- valor: "))
+
+print(valor1 == valor2)
+
+'''
